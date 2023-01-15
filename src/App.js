1cc8b42a58d417014c1bdbd1fb74c1f54cd3a7eb
shambuwu <balfappius@protonmail.ch>
2023-01-15 14:57:21 +0100

    Major rewrite of functional components

--- src/App.js
+++ src/App.js
@@ -14,7 +14,112 @@ import {
 
 const MeasuringStatusContext = createContext(null);
 const UserContext = createContext(null);
+const API_ADDRESS = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`;
 
+function ApiForm(props) {
+    const [context, setContext] = useContext(MeasuringStatusContext);
+    const [status, setStatus] = useState(0);
+
+    async function checkUserStatus(user) {
+        try {
+            let res = await fetch(`${API_ADDRESS}/data/get/${user}/measure_proxy`, {
+                method: "POST",
+            });
+            res.text().then((r) => {
+                setContext(r === 'true');
+                setStatus(1);
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <FormControl
+            sx={{
+                m: 1,
+                display: "inline-block"
+            }}
+            size="small"
+        >
+            <InputLabel id="demo-simple-select-label">User</InputLabel>
+            <Select
+                value={props.user}
+                label="User"
+                onChange={(e) => {
+                    checkUserStatus(e.target.value).then(() => {
+                        props.setUser(e.target.value)
+                    })
+                }}
+                sx={{
+                    minWidth: "200px",
+                    color: "white",
+                    '.MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'rgba(228, 219, 233, 0.25)',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'rgba(228, 219, 233, 0.25)',
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'rgba(228, 219, 233, 0.25)',
+                    },
+                    '.MuiSvgIcon-root ': {
+                        fill: "white !important",
+                    },
+                    'input': {
+                        '&::placeholder': {
+                            textOverflow: 'ellipsis !important',
+                            color: 'blue'
+                        }
+                    }
+                }}
+            >
+                <MenuItem value={"levi"}>Levi</MenuItem>
+                <MenuItem value={"kevin"}>Kevin</MenuItem>
+                <MenuItem value={"geert"}>Geert</MenuItem>
+            </Select>
+            {props.hideTextField ? null : (
+                <TextField
+                    type="text"
+                    value={props.label}
+                    placeholder="Label"
+                    onChange={(e) => props.setLabel(e.target.value)}
+                    size="small"
+                    sx={{
+                        minWidth: "200px",
+                        maxWidth: "200px",
+                        input: {
+                            color: "aliceblue"
+                        },
+                        fieldset: {borderColor: 'rgba(228, 219, 233, 0.25)'},
+                        "& .MuiOutlinedInput-root": {
+                            "&:hover fieldset": {
+                                borderColor: 'rgba(228, 219, 233, 0.25)'
+                            },
+                        },
+                    }}
+                />
+            )}
+            <Button
+                onClick={props.action}
+                type="submit"
+                variant="outlined"
+                sx={props.type === "start" ? {
+                    color: 'rgba(228, 219, 233, 0.50)',
+                    borderColor: 'rgba(228, 219, 233, 0.25)',
+                    minWidth: "200px"
+                } : {
+                    color: '#B90E0A',
+                    borderColor: '#B90E0A',
+                    minWidth: "200px"
+                }}
+            >
+                {props.type === "start" ? "Start Measurement" : "Stop Measurement"}
+            </Button>
+            <FormMessage message={props.message} status={props.status}/>
+        </FormControl>
+    )
+}
 function CustomFadeAlert(props) {
     const [fade, setFade] = useState(false);
 
@@ -44,7 +149,7 @@ function IncomingDataView() {
 
     useEffect(() => {
         let interval = setInterval(() => {
-            fetch(`http://shambuwu.com:8080/data/last_line/${user}`, {
+            fetch(`${API_ADDRESS}/data/last_line/${user}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -64,16 +169,16 @@ function IncomingDataView() {
 
     return (
         <pre>
-            {incomingData ?
+            {incomingData ? (
                 <CustomFadeAlert incomingData={incomingData}/>
-                :
+            ) : (
                 <CircularProgress/>
-            }
+            )}
         </pre>
     )
 }
 
-function FormComponentB() {
+function FormComponent() {
     const [context, setContext] = useContext(MeasuringStatusContext);
     const [user, setUser] = useContext(UserContext);
     const [label, setLabel] = useState("");
@@ -83,7 +188,7 @@ function FormComponentB() {
     let handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            let res = await fetch(`http://shambuwu.com:8080/data/start_measurement/${user}/${label}`, {
+            let res = await fetch(`${API_ADDRESS}/data/start_measurement/${user}/${label}`, {
                 method: "POST",
             });
             if (res.status === 200) {
@@ -100,101 +205,19 @@ function FormComponentB() {
         }
     }
 
-
-    async function checkUserStatus(user) {
-        try {
-            let res = await fetch(`http://shambuwu.com:8080/data/get/${user}/measure_proxy`, {
-                method: "POST",
-            });
-            res.text().then((r) => {
-                setContext(r === 'true');
-                setStatus(1);
-            });
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
     return (
-        <FormControl
-            onSubmit={handleSubmit}
-            sx={{
-                m: 1,
-                display: "inline-block"
-            }}
-            size="small"
-        >
-            <InputLabel id="demo-simple-select-label">User</InputLabel>
-            <Select
-                value={user}
-                label="User"
-                onChange={(e) => {
-                    checkUserStatus(e.target.value).then(() => {
-                        setUser(e.target.value)
-                    })
-                }}
-                sx={{
-                    minWidth: "200px",
-                    color: "white",
-                    '.MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '.MuiSvgIcon-root ': {
-                        fill: "white !important",
-                    },
-                    'input': {
-                        '&::placeholder': {
-                            textOverflow: 'ellipsis !important',
-                            color: 'blue'
-                        }
-                    }
-                }}
-            >
-                <MenuItem value={"levi"}>Levi</MenuItem>
-                <MenuItem value={"kevin"}>Kevin</MenuItem>
-                <MenuItem value={"geert"}>Geert</MenuItem>
-            </Select>
-            <TextField
-                type="text"
-                value={label}
-                placeholder="Label"
-                onChange={(e) => setLabel(e.target.value)}
-                size="small"
-                sx={{
-                    minWidth: "200px",
-                    maxWidth: "200px",
-                    input: {
-                        color: "aliceblue"
-                    },
-                    fieldset: {borderColor: 'rgba(228, 219, 233, 0.25)'},
-                    "& .MuiOutlinedInput-root": {
-                        "&:hover fieldset": {
-                            borderColor: 'rgba(228, 219, 233, 0.25)'
-                        },
-                    },
-                }}
-            />
-            <Button
-                onClick={handleSubmit}
-                type="submit"
-                variant="outlined"
-                sx={{
-                    color: 'rgba(228, 219, 233, 0.50)',
-                    borderColor: 'rgba(228, 219, 233, 0.25)',
-                    minWidth: "200px"
-                }}
-            >
-                Start Measurement
-            </Button>
-            <FormMessage message={message} status={status}/>
-        </FormControl>
-    );
+        <ApiForm
+            user={user}
+            setUser={setUser}
+            hideTextField={false}
+            label={label}
+            setLabel={setLabel}
+            action={handleSubmit}
+            status={status}
+            message={message}
+            type={"start"}
+        />
+    )
 }
 
 function StopMeasureComponent() {
@@ -205,7 +228,7 @@ function StopMeasureComponent() {
 
     async function checkUserStatus(user) {
         try {
-            let res = await fetch(`http://shambuwu.com:8080/data/get/${user}/measure_proxy`, {
+            let res = await fetch(`${API_ADDRESS}/data/get/${user}/measure_proxy`, {
                 method: "POST",
             });
             res.text().then((r) => {
@@ -221,7 +244,7 @@ function StopMeasureComponent() {
     let stopMeasurement = async (e) => {
         e.preventDefault();
         try {
-            let res = await fetch(`http://shambuwu.com:8080/data/stop_measurement/${user}`, {
+            let res = await fetch(`${API_ADDRESS}/data/stop_measurement/${user}`, {
                 method: "POST",
             });
             if (res.status === 200) {
@@ -235,64 +258,14 @@ function StopMeasureComponent() {
     }
 
     return (
-        <FormControl
-            sx={{
-                m: 1,
-                display: "inline-block"
-            }}
-            size="small"
-        >
-            <InputLabel id="demo-simple-select-label">User</InputLabel>
-            <Select
-                size="small"
-                value={user}
-                label="User"
-                onChange={(e) => {
-                    checkUserStatus(e.target.value).then(() => {
-                        setUser(e.target.value)
-                    })
-                }}
-                sx={{
-                    minWidth: "200px",
-                    color: "white",
-                    '.MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'rgba(228, 219, 233, 0.25)',
-                    },
-                    '.MuiSvgIcon-root ': {
-                        fill: "white !important",
-                    },
-                    'input': {
-                        '&::placeholder': {
-                            textOverflow: 'ellipsis !important',
-                            color: 'blue'
-                        }
-                    }
-                }}
-            >
-                <MenuItem value={"levi"}>Levi</MenuItem>
-                <MenuItem value={"kevin"}>Kevin</MenuItem>
-                <MenuItem value={"geert"}>Geert</MenuItem>
-            </Select>
-            <Button
-                onClick={stopMeasurement}
-                type="submit"
-                variant="outlined"
-                sx={{
-                    color: '#B90E0A',
-                    borderColor: '#B90E0A',
-                    minWidth: "200px"
-                }}
-            >
-                Stop Measurement
-            </Button>
-            <FormMessage message={message} status={status}/>
-        </FormControl>
+        <ApiForm
+            user={user}
+            setUser={setUser}
+            hideTextField={true}
+            action={stopMeasurement}
+            status={status}
+            message={message}
+        />
     )
 }
 
@@ -321,12 +294,14 @@ function App() {
             </header>
             <MeasuringStatusContext.Provider value={[measuringStatus, setMeasuringStatus]}>
                 <UserContext.Provider value={[user, setUser]}>
-                    {!measuringStatus ? <FormComponentB/> :
+                    {!measuringStatus ? (
+                        <FormComponent/>
+                    ) : (
                         <>
                             <StopMeasureComponent/>
                             <IncomingDataView/>
                         </>
-                    }
+                    )}
                 </UserContext.Provider>
             </MeasuringStatusContext.Provider>
         </div>
